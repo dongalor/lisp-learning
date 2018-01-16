4ad071ffa401bf295faa07a9984ad682eb38d08b
Paul <p@gstudio.com>
2018-01-17 00:48:24 +0300

    input and function definition

--- lesson1.lisp
+++ lesson1.lisp
@@ -2,6 +2,19 @@
 #||
 hello, my man, this is da big (multiline) comment
 ||#
-(print "Hey, how you doing, Andrew?")
-;; (format t "Hello world ~%")
+(format t "Hello world ~%")
+(print "Hey, how you doing, what's your name?")
+
+;; global variables
+(defvar *name* (read))
+
+;; create a function
+(defun hello-you (*name*)
+  (format t "Hello ~a! ~%" *name*)
+)
+;; capitalization
+(setq *print-case* :capitalize)
+
+(hello-you *name*)
+
 
